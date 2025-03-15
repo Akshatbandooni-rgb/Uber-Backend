f831a25eca8c264c18229bd26e83afaab5527023
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-15 21:12:24 +0530

    Refactor user authentication middleware to use APIError for improved error handling

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,21 +1,27 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
+const APIError = require("../utils/APIError");
 
 // Middleware to authenticate users
 const userAuthentication = async (req, res, next) => {
   try {
-    const token = req.cookies[process.env.COOKIE_NAME]; 
-    if (!token) throw new Error("Access Denied");
+    // Retrieve token from cookies
+    const token = req.cookies[process.env.COOKIE_NAME];
+    if (!token) {
+      return next(new APIError("Unauthorized. Token not found", 401));
+    }
 
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET); 
-    const user = await User.findById(decodedToken._id); 
-
-    if (!user) throw new Error("Token not valid");
-
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decodedToken._id);
+    if (!user) {
+      return next(new APIError("Unauthorized. User not found", 401));
+    }
     req.user = user;
+
+   
     next();
   } catch (error) {
-    next(error)
+    next(new APIError(error.message || "Internal Server Error", error.status || 500));
   }
 };
 
